101b2ad148cf37e10e75e6d75ec821d57f8935aa
edu10 <edu10@192.168.0.130>
2018-06-23 15:52:04 +0530

    gferyh

---
+++ Elixir_aid/src/hospitsl_buildings/Apihyj.java
@@ -0,0 +1,10 @@
+package hospitsl_buildings;
+
+public class Apihyj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Elixir_aid/src/hospitsl_buildings/Ffrej.java
@@ -0,0 +1,10 @@
+package hospitsl_buildings;
+
+public class Ffrej {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
